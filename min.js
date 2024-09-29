c0114daf0930b1f2e7836b817de5beaa52a11618
mostafaZanaty76 <153616846+mostafaZanaty76@users.noreply.github.com>
2024-09-29 19:41:29 +0300

    ,,bg

---
+++ min.js
@@ -0,0 +1,678 @@
+let count = 1;
+let vidBtn=document.querySelectorAll(".video-btn")
+
+
+
+// vidBtn.forEach(slide=>{
+//     slide.addEventListener("click",function(){
+//         document.querySelector(".controls .blue").classList.remove("blue")
+//         slide.classList.add("blue")
+//       //   let src =slide.getAttribute("data-src");
+//       //   document.querySelector("#video-slider").src =src
+//     })
+// })
+function tasbeeh() {
+     if (count!=0 ){
+    count--;
+    document.getElementById('tasbeehCount').innerText = count;
+
+}
+    return ;
+
+    
+}
+let count1 = 1;
+function tasbeeh1() {
+    if (count1!=0 ){
+   count1--;
+   document.getElementById('tasbeehCount1').innerText = count1;
+
+}
+   return ;
+
+   
+}
+let count2 = 1;
+function tasbeeh2() {
+    if (count2!=0 ){
+   count2--;
+   document.getElementById('tasbeehCount2').innerText = count2;
+
+}
+   return ;
+
+   
+}
+let count3 = 1;
+function tasbeeh3() {
+    if (count3!=0 ){
+   count3--;
+   document.getElementById('tasbeehCount3').innerText = count3;
+
+}
+   return ;
+
+   
+}
+let count4 = 1;
+function tasbeeh4() {
+    if (count4!=0 ){
+   count4--;
+   document.getElementById('tasbeehCount4').innerText = count4;
+
+}
+   return ;
+
+   
+}
+let count5 = 1;
+function tasbeeh5() {
+    if (count5!=0 ){
+   count5--;
+   document.getElementById('tasbeehCount5').innerText = count5;
+
+}
+   return ;
+
+   
+}
+let count6 = 1;
+function tasbeeh6() {
+    if (count6!=0 ){
+   count6--;
+   document.getElementById('tasbeehCount6').innerText = count6;
+
+}
+   return ;
+
+   
+}
+let count7 = 1;
+function tasbeeh7() {
+    if (count7!=0 ){
+   count7--;
+   document.getElementById('tasbeehCount7').innerText = count7;
+
+}
+   return ;
+
+   
+}
+let count8 = 1;
+function tasbeeh8() {
+    if (count8!=0 ){
+   coun8t--;
+   document.getElementById('tasbeehCount8').innerText = count8;
+
+}
+   return ;
+
+   
+}
+let count9 = 1;
+function tasbeeh9() {
+    if (count9!=0 ){
+   count9--;
+   document.getElementById('tasbeehCount9').innerText = count9;
+
+}
+   return ;
+
+   
+}
+let count10 = 1;
+function tasbeeh10() {
+    if (count10!=0 ){
+   count10--;
+   document.getElementById('tasbeehCount10').innerText = count10;
+
+}
+   return ;
+
+   
+}
+let count11 = 1;
+function tasbeeh11() {
+    if (count11!=0 ){
+   count11--;
+   document.getElementById('tasbeehCount11').innerText = count11;
+
+}
+   return ;
+
+   
+}
+
+let count33 = 33;
+function tasbeeh33() {
+    if (count33!=0 ){
+   count33--;
+   document.getElementById('tasbeehCount33').innerText = count33;
+
+}
+   return ;
+
+   
+}
+let count34 = 33;
+function tasbeeh34() {
+    if (count34!=0 ){
+   count34--;
+   document.getElementById('tasbeehCount34').innerText = count34;
+
+}
+   return ;
+
+   
+}
+let count35 = 33;
+function tasbeeh35() {
+    if (count35!=0 ){
+   count35--;
+   document.getElementById('tasbeehCount35').innerText = count35;
+
+}
+   return ;
+
+   
+}
+
+
+
+
+let uncount = 100;
+
+function tasbeh() {
+     if (uncount!=0 ){
+    uncount--;
+    document.getElementById('tasbehCount').innerText = uncount;
+
+}
+    return ;
+
+    
+}
+let uncount01 = 100;
+function tasbeh1() {
+     if (uncount01!=0 ){
+    uncount01--;
+    document.getElementById('tasbehCount01').innerText = uncount01;
+
+}
+    return ;
+
+    
+}
+let uncount02 = 100;
+function tasbeh2() {
+    if (uncount02!=0 ){
+   uncount02--;
+   document.getElementById('tasbehCount02').innerText = uncount02;
+
+}
+   return ;
+
+   
+}
+let uncount03 = 100;
+function tasbeh3() {
+    if (uncount03!=0 ){
+   uncount03--;
+   document.getElementById('tasbehCount03').innerText = uncount03;
+
+}
+   return ;
+
+   
+}
+let uncount04 = 100;
+function tasbeh4() {
+    if (uncount04!=0 ){
+   uncount04--;
+   document.getElementById('tasbehCount04').innerText = uncount04;
+
+}
+   return ;
+
+   
+}
+let uncount05 = 100;
+function tasbeh5() {
+    if (uncount05!=0 ){
+   uncount05--;
+   document.getElementById('tasbehCount05').innerText = uncount05;
+
+}
+   return ;
+
+   
+}
+let uncount06 = 100;
+function tasbeh6() {
+    if (uncount06!=0 ){
+   uncount06--;
+   document.getElementById('tasbehCount06').innerText = uncount06;
+
+}
+   return ;
+
+   
+}
+let uncount07 = 100;
+function tasbeh7() {
+    if (uncount07!=0 ){
+   uncount07--;
+   document.getElementById('tasbehCount07').innerText = uncount07;
+
+}
+   return ;
+
+   
+}
+let uncount08 = 100;
+function tasbeh8() {
+    if (uncount08!=0 ){
+   uncount08--;
+   document.getElementById('tasbehCount08').innerText = uncount08;
+
+}
+   return ;
+
+   
+}
+let uncount09 = 100;
+function tasbeh9() {
+    if (uncount09!=0 ){
+   uncount09--;
+   document.getElementById('tasbehCount09').innerText = uncount09;
+
+}
+   return ;
+
+   
+}
+let uncount010 = 100;
+function tasbeh10() {
+    if (uncount010!=0 ){
+   uncount010--;
+   document.getElementById('tasbehCount010').innerText = uncount010;
+
+}
+   return ;
+
+   
+}
+let uncount011 = 100;
+function tasbeh11() {
+    if (uncount011!=0 ){
+   uncount011--;
+   document.getElementById('tasbehCount011').innerText = uncount011;
+
+}
+   return ;
+
+   
+}
+let uncount012 = 100;
+function tasbeh12() {
+    if (uncount012!=0 ){
+   uncount012--;
+   document.getElementById('tasbehCount012').innerText = uncount012;
+
+}
+   return ;
+
+   
+}
+let uncount013 = 100;
+function tasbeh13() {
+    if (uncount013!=0 ){
+   uncount013--;
+   document.getElementById('tasbehCount013').innerText = uncount013;
+
+}
+   return ;
+
+   
+}
+let uncount014 = 100;
+function tasbeh14() {
+    if (uncount014!=0 ){
+   uncount014--;
+   document.getElementById('tasbehCount014').innerText = uncount014;
+
+}
+   return ;
+
+   
+}
+let uncount015 = 100;
+function tasbeh15() {
+    if (uncount015!=0 ){
+   uncount015--;
+   document.getElementById('tasbehCount015').innerText = uncount015;
+
+}
+   return ;
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+let count00 = 3;
+
+function tasbh00() {
+     if (count00!=0 ){
+    count00--;
+    document.getElementById('tasbhCount0').innerText = count00;
+
+}
+    return ;
+
+    
+}
+let count01 = 3;
+
+function tasbh01() {
+     if (count01!=0 ){
+    count01--;
+    document.getElementById('tasbhCount01').innerText = count01;
+
+}
+    return ;
+
+    
+}
+let count02 = 3;
+
+function tasbh02() {
+     if (count02!=0 ){
+    count02--;
+    document.getElementById('tasbhCount02').innerText = count02;
+
+}
+    return ;
+
+    
+}
+let count03 = 3;
+
+function tasbh03() {
+     if (count03!=0 ){
+    count03--;
+    document.getElementById('tasbhCount03').innerText = count03;
+
+}
+    return ;
+
+    
+}
+let count04 = 3;
+
+function tasbh04() {
+     if (count04!=0 ){
+    count04--;
+    document.getElementById('tasbhCount04').innerText = count04;
+
+}
+    return ;
+
+    
+}
+let count05 = 3;
+
+function tasbh05() {
+     if (count05!=0 ){
+    count05--;
+    document.getElementById('tasbhCount05').innerText = count05;
+
+}
+    return ;
+
+    
+}
+let count06 = 3;
+
+function tasbh06() {
+     if (count06!=0 ){
+    count06--;
+    document.getElementById('tasbhCount06').innerText = count06;
+
+}
+    return ;
+
+    
+}
+let count07 = 3;
+
+function tasbh07() {
+     if (count07!=0 ){
+    count07--;
+    document.getElementById('tasbhCount07').innerText = count07;
+
+}
+    return ;
+
+    
+}
+let count08 = 3;
+
+function tasbh08() {
+     if (count08!=0 ){
+    count08--;
+    document.getElementById('tasbhCount08').innerText = count08;
+
+}
+    return ;
+
+    
+}
+
+let count09 = 3;
+
+function tasbh09() {
+     if (count09!=0 ){
+    count09--;
+    document.getElementById('tasbhCount09').innerText = count09;
+
+}
+    return ;
+
+    
+}
+
+let count010 = 3;
+
+function tasbh010() {
+     if (count010!=0 ){
+    count010--;
+    document.getElementById('tasbhCount010').innerText = count010;
+
+}
+    return ;
+
+    
+}
+
+let count011 = 3;
+
+function tasbh011() {
+     if (count011!=0 ){
+    count011--;
+    document.getElementById('tasbhCount011').innerText = count011;
+
+}
+    return ;
+
+    
+}
+let count012 = 3;
+
+function tasbh012() {
+     if (count012!=0 ){
+    count012--;
+    document.getElementById('tasbhCount012').innerText = count012;
+
+}
+    return ;
+
+    
+}
+let count013 = 3;
+
+function tasbh013() {
+     if (count012!=0 ){
+    count012--;
+    document.getElementById('tasbhCount013').innerText = count013;
+
+}
+    return ;
+
+    
+}
+let count014 = 3;
+
+function tasbh014() {
+     if (count014!=0 ){
+    count014--;
+    document.getElementById('tasbhCount014').innerText = count014;
+
+}
+    return ;
+
+    
+}
+
+
+
+let count001 = 4;
+function tasb001() {
+    if (count001!=0 ){
+   count001--;
+   document.getElementById('tasbCount001').innerText = count001;
+
+}
+   return ;
+
+   
+}
+let count002 = 4;
+function tasb002() {
+    if (count002!=0 ){
+   count002--;
+   document.getElementById('tasbCount002').innerText = count002;
+
+}
+   return ;
+
+   
+}
+let count003 = 4;
+function tasb003() {
+    if (count003!=0 ){
+   count003--;
+   document.getElementById('tasbCount003').innerText = count003;
+
+}
+   return ;
+
+   
+}
+let count004 = 4;
+function tasb004() {
+    if (count004!=0 ){
+   count004--;
+   document.getElementById('tasbCount004').innerText = count004;
+
+}
+   return ;
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let count0007 = 7;
+function eeh() {
+    if (count0007!=0 ){
+   count0007--;
+   document.getElementById('eehCount').innerText = count0007;
+
+}
+   return ;
+
+   
+}
+
+
+let count00010 = 10;
+function bh() {
+    if (count00010!=0 ){
+   count00010--;
+   document.getElementById('bhCount').innerText = count00010;
+
+}
+   return ;
+
+   
+}
+// function myFunction() {
+//     console.log("1");
+//     // يمكنك وضع الشروط هنا
+//     if () {
+//         console.log(0);
+//         return; // سيتوقف تنفيذ الوظيفة هنا
+//     }
+//     console.log("0");
+// }
+
+// myFunction();
+
+
+// for     (let i = 0; i < 10; i++) {
+//     console.log(i);
+//     if (i === 5) {
+//         break; // سيتوقف التكرار عندما يكون i يساوي 5
+//     }
+// }
